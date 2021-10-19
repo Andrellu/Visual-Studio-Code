b7aa20eb6cec6dbc0ff823f76db0030c65868a6e
Andrellu <andrellu.o.f@gmail.com>
2021-10-19 17:10:36 +0200

    19 de octubre

--- React/primerreact/src/components/Padre/PadreDibujo.js
+++ React/primerreact/src/components/Padre/PadreDibujo.js
@@ -15,9 +15,9 @@ function PadreDibujo(){
             <h1 style={{color:"green"}}>La multiplicacion será : {Total}</h1>
             <div>
                 <MatematicasDibujo NumeroUno="2" NumeroDos="5" metodoPadre={multiplicarPadre}/>
-                <MatematicasDibujo NumeroUno="5" NumeroDos="5" metodoPadre={multiplicarPadre}/>
-                <MatematicasDibujo NumeroUno="4" NumeroDos="5" metodoPadre={multiplicarPadre}/>
                 <MatematicasDibujo NumeroUno="3" NumeroDos="5" metodoPadre={multiplicarPadre}/>
+                <MatematicasDibujo NumeroUno="4" NumeroDos="5" metodoPadre={multiplicarPadre}/>
+                <MatematicasDibujo NumeroUno="5" NumeroDos="5" metodoPadre={multiplicarPadre}/>
             </div>
         </div>
     );
