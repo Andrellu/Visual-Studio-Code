a36d6c452003ecb9b4da0f22d3ebe94033d8881a
Andrellu <andrellu.o.f@gmail.com>
2021-10-19 09:28:39 +0200

    19 de octubre

--- React/ejemploreact/src/Components/Ejemplo/Ejemplo.js
+++ React/ejemploreact/src/Components/Ejemplo/Ejemplo.js
@@ -1,18 +1,20 @@
 import foto from './../../assets/images/ImagenDos.jpg';
 function Ejemplo(){
-    var numUno = 1;
-    var numDos = 2;
 
-    const sumar = () =>{
-        var suma = numUno + numDos;
-        console.log(suma);
+    const sumar = (numero1, numero2) =>{
+        var suma = numero1 + numero2;
+        console.log("La suma es :" + suma);
     }
+    var imgSize = {
+        width: "150px",
+        height: "150px"
+    };
 
     return (
         <div>
-            <h1> {numUno} + {numDos} </h1>
-            <button onClick={() => sumar()}>Sumar</button>
-            <img src={foto}></img>
+            <h1> 1 + 2 </h1>
+            <button onClick={() => sumar(1,2)}>Sumar</button>
+            <img style={imgSize} className="App-logo" src={foto}></img>
         </div>
     );
 }
