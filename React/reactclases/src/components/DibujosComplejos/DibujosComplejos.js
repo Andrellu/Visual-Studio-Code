828911c3f615c85497cf9886c0101f864f017cbe
Andrellu <andrellu.o.f@gmail.com>
2021-10-20 13:48:47 +0200

    20 de octubre

--- React/reactclases/src/components/DibujosComplejos/DibujosComplejos.js
+++ React/reactclases/src/components/DibujosComplejos/DibujosComplejos.js
@@ -2,10 +2,27 @@ import { Component } from "react";
 
 class DibujosComplejos extends Component{
 
+    dibujarNumeros = () =>{
+        // SI TENEMOS UN BUCLE VAMOS A VISUALIZAR EL DIBUJO
+        // NOS OFRECE COMO RESULTADO. HAY QUE ALMACENAR DENTRO DE 
+        // UN LIST TODO EL CONTENIDO HTML
+        // UNA LISTA SE DECLARA COMO UN ARRAY list = []
+        // TIENE UN METOD PUSH PARA AÑADIR ELEMENTOS A LA 
+        // LISTA
+        var lista = [];
+        for(var i = 1; i <=5; i++){
+            lista.push(<li>Numero {i}</li>);
+        }
+        return lista;
+    }
+
     render (){
         return (
-            <div className="App"> 
-                <h1>Dibujos con bucles</h1>
+            <div> 
+                <h1 className="App">Dibujos con bucles</h1>
+                <ul>
+                    {this.dibujarNumeros()}
+                </ul>
             </div>
         );
     }
