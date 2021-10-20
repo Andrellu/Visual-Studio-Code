80cf8b4c09560ff791cce634d108bc99668833ee
Andrellu <andrellu.o.f@gmail.com>
2021-10-20 09:12:50 +0200

    20 de octubre

--- React/primerreact/src/components/Padre/PadreDibujo.js
+++ React/primerreact/src/components/Padre/PadreDibujo.js
@@ -6,13 +6,18 @@ function PadreDibujo(){
 
     const [Total, setToatal] = useState(0);
 
+    const [numUno, setNumUno] = useState(0);
+    const [numDos, setNumDos] = useState(0);
+
     const multiplicarPadre = (NumeroUno, NumeroDos) => {
         setToatal(NumeroUno*NumeroDos);
+        setNumUno(NumeroUno);
+        setNumDos(NumeroDos);
     }
 
     return (
         <div>
-            <h1 style={{color:"green"}}>La multiplicacion será : {Total}</h1>
+            <h1 style={{color:"green"}}>La multiplicacion de {numUno} x {numDos} será : {Total}</h1>
             <div>
                 <MatematicasDibujo NumeroUno="2" NumeroDos="5" metodoPadre={multiplicarPadre}/>
                 <MatematicasDibujo NumeroUno="3" NumeroDos="5" metodoPadre={multiplicarPadre}/>
