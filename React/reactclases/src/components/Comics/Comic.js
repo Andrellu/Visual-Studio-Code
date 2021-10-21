732f7d3babbf4fa6fc1e813e6d224e097864cfa6
Andrellu <andrellu.o.f@gmail.com>
2021-10-21 14:01:20 +0200

    21 de octubre

--- React/reactclases/src/components/Comics/Comic.js
+++ React/reactclases/src/components/Comics/Comic.js
@@ -2,13 +2,27 @@ import { Component } from "react";
 
 class Comic extends Component{
 
-        render () {
-            return (
-                <div>
-                    <h1 style={{color:"fuchsia"}}>Ejemplo comics Objetos/Colecciones</h1>
-                    <h2>Comic Hijo</h2>
-                </div>
-            );
-        }
+    seleccionFavorito = () =>{
+        var seleccionado = this.props.comic;
+        this.props.metodoPadre(seleccionado);
+    }
+
+    elimnarComic = () =>{
+        var indice = this.props.comic.indice;
+        this.props.metodoEliminar(indice);
+    }
+
+    render () {
+        return (
+            <div style={{backgroundColor:"#69D2E7"}}>
+                <h1 style={{color:"Blue"}}>{this.props.comic.titulo}</h1>
+                <h6 style={{color:"orange"}}>{this.props.comic.descripcion}</h6>
+                <img src={this.props.comic.imagen}/>
+                <button onClick={this.seleccionFavorito}>Favorito</button>
+                <button onClick={this.elimnarComic}>Eliminar</button>
+            </div>
+
+        );
+    }
 }
 export default Comic;
