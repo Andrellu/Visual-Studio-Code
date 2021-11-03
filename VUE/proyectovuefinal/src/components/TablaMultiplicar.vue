6aec3000c9e426e62333b8ec5b8101e4e1e97045
Andrellu <andrellu.o.f@gmail.com>
2021-11-03 10:34:05 +0100

    3 de noviembre

--- VUE/proyectovuefinal/src/components/TablaMultiplicar.vue
+++ VUE/proyectovuefinal/src/components/TablaMultiplicar.vue
@@ -1,34 +1,54 @@
 <template>
-    <div>
-        <main class="row justify-content-center">
-            <input type="number" v-model="numero"/>
-            <h1>Tabla de multiplicar del: {{numero}}</h1>
-            <hr/>
-            <table border="2">
-                <thead>
-                    <tr>
-                        <th>Operación</th>
-                        <th>Resultado</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr  v-for="i in 10" :key="i">
-                        <td>{{numero}} x {{i}}</td>
-                        <td>{{numero*i}}</td>
-                </tr>
-                </tbody>
-            </table>
-        </main>
-    </div>
-</template>
+  <div>
+    <h1>Tabla de multiplicar de {{num}}</h1>
+    <label>Introduzca un numero: </label>
+    <input type="number" v-model="num" />
+    <br />
+    <button type="button" @click="calcularTabla()">
+      Mostrar tabla de multiplicar
+    </button>
+    <hr />
 
+    <table id="tabla" border="2">
+      <thead>
+        <tr>
+            <th style="color:green">Operación</th>
+            <th style="color:green">Resultado</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="i in solucion" :key="i" v-html="i">{{i}}</tr>
+        <!-- <tr v-for="i in 10" :key="i">
+            <td>{{ num }} x {{ i }}</td>
+            <td>{{ num * i }}</td>
+        </tr> -->
+      </tbody>
+    </table>
+  </div>
+</template>
 <script>
-    export default {
-        name : "TablaMultiplicar",
-        data(){
-            return{
-                numero : 0
-            }
-        }
-    }
+export default {
+  name: "TablaMultiplicar",
+  data() {
+    return {
+      num: 0,
+      solucion: [],
+    };
+  },
+  methods: {
+    calcularTabla() {
+        //Se vacia el array antes de hacer nada
+        this.solucion = [];
+      // SOLO UN ARRAY :)
+      for (var i = 0; i <= 10; i++) {
+        var solu = parseInt(this.num) * i;
+        var dibujo =
+            "<td style='color:red'>" + this.num + "x" + i + "</td>" + "<td style='color:blue'>" + solu + "</td>";
+        this.solucion.push(dibujo);
+      }
+      // ESTARIA GUAY UN RETURN
+      return this.solucion;
+    },
+  },
+};
 </script>
