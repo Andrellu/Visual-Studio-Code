05bd06826cbcf67b3cbdeb38cdf17f647fb2cc6b
Andrellu <andrellu.o.f@gmail.com>
2021-11-05 12:40:22 +0100

    5 de noviembre

--- VUE/proyectoapivue/src/App.vue
+++ VUE/proyectoapivue/src/App.vue
@@ -1,8 +1,9 @@
 <template>
-    <div>
+    <div id="app">
       <Menu/>
       <hr/>
     <router-view></router-view>
+    <img src="./assets/logo.png" alt="Vue logo"/>
   </div>
 </template>
 
@@ -16,3 +17,14 @@ export default {
   }
 }
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
