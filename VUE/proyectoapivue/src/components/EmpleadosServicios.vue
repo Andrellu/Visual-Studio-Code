6a6a7d163e8525b7ffd6ddae1d87bf6b74e9212e
Andrellu <andrellu.o.f@gmail.com>
2021-11-06 11:43:19 +0100

    6 de noviembre

--- VUE/proyectoapivue/src/components/EmpleadosServicios.vue
+++ VUE/proyectoapivue/src/components/EmpleadosServicios.vue
@@ -4,33 +4,59 @@
         <div class="row justify-content-center">
             <div class="col-8 text-center">
                 <form class="form-control">
-                    <select class="form-control" v-model="oficio">
-                        <option v-for="(oficio,index) in oficios" :key="index">{{oficio}}</option>
+                    <select class="form-control" v-model="oficio" @change.prevent="cargarEmpleadosOficio">
+                        <option :value="oficio" v-for="(oficio,index) in oficios" :key="index">{{oficio}}</option>
                     </select>
                 </form>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-8 text-center" v-if="empleadosOficios.length != 0">
+                <table border="2" class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>APELLIDO</th>
+                            <th>OFICIO</th>
+                            <th>SALARIO</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="(empleado,index) in empleadosOficios" :key="index">
+                            <td>{{empleado.apellido}}</td>
+                            <td>{{empleado.oficio}}</td>
+                            <td>{{empleado.salario}}</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
 </template>
 <script>
 import ServiceEmpleado from "./../services/ServiciosEmpleados";
-const servive = new ServiceEmpleado();
+const service = new ServiceEmpleado();
 export default {
     name:"EmpleadosServicios",
     data(){
         return {
             oficios : [],
-            oficio : ""
+            oficio : "",
+            empleadosOficios : []
         }
     },
     mounted(){
-        servive.getEmpleados().then(res => {
-            console.log(res);
+        service.getEmpleados().then(res => {
+            //console.log(res);
             this.oficios = res;
         });
     },
     methods : {
-        //servive.
+        cargarEmpleadosOficio(){
+            service.cargarEmpleadosOficio(this.oficio).then(res => {
+                console.log(res);
+                this.empleadosOficios = res;
+            });
+        }
     }
 }
 </script>
