1951c75ed11f8416f00844b8aea3e8846dd59791
Andrellu <andrellu.o.f@gmail.com>
2021-11-11 11:52:55 +0100

    11 de noviembre

--- Angular/primerproyecto/src/components/deportes/deportes.component.ts
+++ Angular/primerproyecto/src/components/deportes/deportes.component.ts
@@ -8,8 +8,11 @@ import { Component } from "@angular/core";
 export class Deportes{
     
     public sports : Array<string>;
+    public numeros : Array<number>;
 
     constructor(){
         this.sports = ["Baloncesto","Curling","Fútbol","Golf","Tenis","Padel"];
+        //this.sports = ["Baloncesto","Curling","Fútbol","Golf","Tenis","Padel","Volley-Ball"];
+        this.numeros = [1,2,3,4,5,6,7,8,9,10];
     }
 }
