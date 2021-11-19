1705cd375571bae14e509b81829436d45c84aff9
Andrellu <andrellu.o.f@gmail.com>
2021-11-19 14:17:23 +0100

    19 de noviembre

--- VUE/examenvueaof/src/components/Peliculas/buscarPelicula.vue
+++ VUE/examenvueaof/src/components/Peliculas/buscarPelicula.vue
@@ -1,11 +1,49 @@
 <template>
   <div>
-    
+    <div>
+      <div>
+          <table v-if="jugadores">
+              <thead>
+                  <tr>
+                      <th>Nombre</th>
+                      <th>Posicion</th>
+                      <th>Imagen</th>
+                  </tr>
+              </thead>
+              <tbody>
+                  <tr v-for="(peli, index) in peliculas" :key="index">
+                      <td>{{peli.nombre}}</td>
+                      <td>{{peli.posicion}}</td>
+                      <td><img :src="peli.foto" alt="imagen"/></td>
+                  </tr>
+              </tbody>
+          </table>
+      </div>
+  </div>
   </div>
 </template>
 
 <script>
+import ServicePeliculas from './../../components/Services/Peliculas';
+const service = new ServicePeliculas();
 export default {
+  name : "BuscarPelicula",
+  mounted(){
+
+  },
+  methods:{
+    cargarPeliculas(){
+      service.getPeliculasNombre().then(res=>{
+        this.peliculas = res;
+      });
+    }
+  },
+  data(){
+    return {
+      nombre : "",
+      peliculas : []
+    }
+  }
 
 }
 </script>
